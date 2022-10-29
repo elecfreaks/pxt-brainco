e2cfe4e8d2f3716588fda65c80b7fffa11edf7e2
Xu Hao <xuhow@outlook.com>
2022-10-29 14:05:52 +0800

    Add some blocks

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
-
+//%color=#921AFF icon="\uf076" block="Brainco"
+namespace brainco {
+    /**
+     * Get the current concentration value from Brainco.
+     */
+    //% block="get concentration" blockId="brainco.getConcentration"
+    export function getConcentration(): number {
+        return serial.readBuffer(1)[0];
+    }
+}
